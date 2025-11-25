9733e10336e42de3f79ff69a44d1dae37ac81939
Rohitverma9569 <131365995+Rohitverma9569@users.noreply.github.com>
2025-11-25 16:50:55 +0530

    Time: 45 ms (81.56%) | Memory: 24.5 MB (58.67%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -3,7 +3,7 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,   greater<pair<int,pair<int,int>>>>pq;
         vector<vector<int>> dist(n,vector<int>(m,INT_MAX));
         dist[0][0]=0;
         pq.push({0,{0,0}});
@@ -15,7 +15,7 @@ public:
             int xcor=it.second.first;
             int ycor=it.second.second; 
             pq.pop();
-            if(xcor==n-1  && ycor==m-1) return dis;
+            if(xcor==n-1 && ycor==m-1) return dis;
             for(int i=0;i<4;i++){
                 int row=xcor+xt[i];
                 int col=ycor+yt[i];
@@ -24,15 +24,11 @@ public:
                     if(newEffort<dist[row][col]){
                         dist[row][col]=newEffort;
                         pq.push({newEffort,{row,col}});
+                        
                     }
                 }
             }
-
         }
-        return 0;
-
-
-        
-         
+        return 0;     
     }
 };

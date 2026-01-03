6182cc22fe9a66984a63c32c0656bf94d3c6b6a3
Rohitverma9569 <131365995+Rohitverma9569@users.noreply.github.com>
2026-01-04 01:51:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (0.00%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.cpp
+++ 4177-reverse-string-prefix/reverse-string-prefix.cpp
@@ -2,8 +2,15 @@ class Solution {
 public:
     string reversePrefix(string s, int k) {
         int n=s.size();
-        reverse(s.begin(),s.begin()+k);
-        return s;
+        string ans="";
+        for(int i=k-1;i>=0;i--){
+            ans+=s[i];
+
+        }
+        for(int i=k;i<n;i++){
+            ans+=s[i];
+        }
+        return ans;
 
         
     }

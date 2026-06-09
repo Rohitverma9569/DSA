332e16502faba87ef4222e3a833255ea031796c3
Rohit Verma <131365995+Rohitverma9569@users.noreply.github.com>
2026-06-09 09:08:55 +0530

    Time: 4 ms (33.07%) | Memory: 103.8 MB (32.68%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -5,9 +5,9 @@ public:
         long long maxi = *max_element(nums.begin(), nums.end());
         long long mini = *min_element(nums.begin(),nums.end());
         
-        long long  ele=maxi-mini;
-        long long ans=k*ele;
-        return ans;
+        long long  ele=(maxi-mini)*k;
+        
+        return ele;
            
     }
 };

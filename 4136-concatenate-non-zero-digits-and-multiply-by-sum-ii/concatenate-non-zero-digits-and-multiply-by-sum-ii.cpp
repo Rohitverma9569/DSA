ac6997948bab1945ad01b1119024b76f8dd717b9
Rohit Verma <131365995+Rohitverma9569@users.noreply.github.com>
2026-07-08 14:42:46 +0530

    Time: 40 ms (61.66%) | Memory: 155.2 MB (67.36%) - LeetSync

---
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+int mod=1e9+7;
+typedef long long ll;
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n=s.length();
+        vector<int> nonZeroCount(n,0);
+        vector<ll> numberUpTo(n,0);
+        vector<ll> digitSumUpTo(n,0);
+        vector<ll> pow10(n+1,0);
+
+        pow10[0]=1;
+        for(int i=1;i<n;i++){
+            pow10[i]=(pow10[i-1]*10)%mod;
+        }
+        nonZeroCount[0]=(s[0]=='0')?0:1;
+        for(int i=1;i<n;i++){
+            int digit=s[i]-'0';
+            nonZeroCount[i]=nonZeroCount[i-1]+((digit!=0)?1:0);
+
+
+        }
+        numberUpTo[0]=s[0]-'0';
+        for(int i=1;i<n;i++){
+            int digit=s[i]-'0';
+            if(digit!=0){
+                numberUpTo[i]=((numberUpTo[i-1]*10) + digit)%mod;
+            }else{
+                numberUpTo[i]=numberUpTo[i-1];
+            }
+        }
+        digitSumUpTo[0]=s[0]-'0';
+        for(int i=1;i<n;i++){
+            int digit=s[i]-'0';
+            digitSumUpTo[i]=digitSumUpTo[i-1]+digit;
+        }
+
+        int q=queries.size();
+        vector<int> result(q);
+        for(int i=0;i<q;i++){
+            int l=queries[i][0];
+            int r=queries[i][1];
+            ll sum=digitSumUpTo[r]-((l==0)?0:digitSumUpTo[l-1]);
+
+            int numBefore= (l==0)?0:numberUpTo[l-1];
+            int k=nonZeroCount[r]-((l==0)?0:nonZeroCount[l-1]);
+            ll x= (numberUpTo[r]-(numBefore*pow10[k] % mod)+mod)%mod;
+            result[i]=(int)((x*sum)%mod);
+
+        }
+        return result;
+        
+    }
+};

b2db6965b883f564149f6531046c5252dabbc82c
Rohit Verma <131365995+Rohitverma9569@users.noreply.github.com>
2026-08-03 20:29:52 +0530

    Time: 36 ms (48.00%) | Memory: 141.6 MB (43.55%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+vector<int>dp;
+    int solve(vector<int>& stoneValue, int i){
+        int n=stoneValue.size();
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int result=stoneValue[i]-solve(stoneValue,i+1);
+        if(i+1<n){
+
+        
+          result=max(result,stoneValue[i]+stoneValue[i+1]-solve(stoneValue,i+2));
+        }
+        if(i+2<n){
+        result=max(result,stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-solve(stoneValue,i+3));
+        }
+        return dp[i]=result;
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n=stoneValue.size();
+        dp.resize(n+1,-1);
+        int diff=solve(stoneValue,0);
+        if(diff>0){
+            return "Alice";
+        }
+        if(diff<0){
+            return "Bob";
+        }
+        return "Tie";
+
+
+        
+    }
+};

80bcadc18d7e505c24250926f81c7ac06e081bd5
Rohit Verma <131365995+Rohitverma9569@users.noreply.github.com>
2026-09-20 22:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (78.39%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            int k=ch-'a'+1;
+            int a=27-k;
+            ans+=a*(i+1);
+        }
+        return ans;
+        
+    }
+};
